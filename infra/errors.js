91df01c2ca2683eeba5bb55b6edf5af13b543492
Aline Souza <143272482+alinevsouza@users.noreply.github.com>
2025-07-03 21:29:33 +0000

    feat: add `MethodNotAllowesError` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,9 +1,45 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("Erro interno inesperado aconteceu.", { cause });
     this.name = "InternalServerError";
     this.action = "Contate o suporte informando o erro.";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método inválido para este endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action = "Verifique se o método HTTP utilizado está correto.";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Serviço indisponível no momento.", { cause });
+    this.name = "ServiceError";
+    this.action = "Verifique se o serviço está disponível.";
+    this.statusCode = 503;
   }
 
   toJSON() {
