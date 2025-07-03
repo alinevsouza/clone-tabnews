6a394d7d54dd7c12c7f3ccd99fee813877f7d174
Aline Souza <143272482+alinevsouza@users.noreply.github.com>
2025-07-03 21:28:21 +0000

    feat: use `ServiceError` in `database.js`

--- infra/database.js
+++ infra/database.js
@@ -1,4 +1,5 @@
 import { Client } from "pg";
+import { ServiceError } from "./errors.js";
 
 async function query(queryObject) {
   let client;
@@ -7,8 +8,11 @@ async function query(queryObject) {
     const result = await client.query(queryObject);
     return result;
   } catch (error) {
-    console.error(error);
-    throw error;
+    const serviceError = new ServiceError({
+      message: "Erro ao executar conexão ou consulta no banco de dados.",
+      cause: error,
+    });
+    throw serviceError;
   } finally {
     await client?.end();
   }
