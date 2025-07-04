89cd56f27a279bedc15f89c37ead0c9c0ecd8042
Aline Souza <143272482+alinevsouza@users.noreply.github.com>
2025-07-04 17:49:30 +0000

    feat: create and use `notfounderror` in `controller.js`

--- infra/errors.js
+++ infra/errors.js
@@ -69,3 +69,21 @@ export class ValidationError extends Error {
     };
   }
 }
+
+export class NotFoundError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "Recurso não encontrado.", { cause });
+    this.name = "NotFoundError";
+    this.action = action || "Verifique se os dados informados estão corretos.";
+    this.statusCode = 404;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
