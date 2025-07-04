ae07e66024203a98137c27fa315b6bc42f4c5e55
Aline Souza <143272482+alinevsouza@users.noreply.github.com>
2025-07-04 00:15:09 +0000

    feat: add migration to create `users` table

---
+++ infra/migrations/1751586334344_users.js
@@ -0,0 +1,47 @@
+exports.up = (pgm) => {
+  pgm.createTable("users", {
+    id: {
+      type: "uuid",
+      primaryKey: true,
+      default: pgm.func("gen_random_uuid()"),
+    },
+
+    // FYI: GitHub's usernames to 39 characters.
+    username: {
+      type: "varchar(30)",
+      notNull: true,
+      unique: true,
+    },
+
+    // FYI: The maximum length of an email address is 254 characters.
+    // Source: https://stackoverflow.com/a/1199238
+    email: {
+      type: "varchar(254)",
+      notNull: true,
+      unique: true,
+    },
+
+    // FYI: The maximum length of a hash password is 60 characters.
+    // Source: https://www.npmjs.com/package/bcrypt#hash-info
+    password_hash: {
+      type: "varchar(60)",
+      notNull: true,
+    },
+
+    // FYI: Timestamp with time zone is used to store the date and time.
+    // Source: https://justatheory.com/2012/04/postgres-use-timestamptz/
+    created_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+
+    updated_at: {
+      type: "timestamptz",
+      notNull: true,
+      default: pgm.func("timezone('utc', now())"),
+    },
+  });
+};
+
+exports.down = false;
